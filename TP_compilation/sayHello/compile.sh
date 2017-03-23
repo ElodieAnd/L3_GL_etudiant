0fa766bb98c2258b6125bd42ff16743f1327727b
Elodie And <elodieandrieux62100@gmail.com>
2017-03-23 15:29:03 +0100

    Documentation 2

---
+++ TP_compilation/sayHello/compile.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+g++ -c -g module1.cpp -std=c++11
+g++ -c -g sayHello.cpp -std=c++11
--- TP_documentation/drunk_player/src/mainpage.hpp
+++ TP_documentation/drunk_player/src/mainpage.hpp
@@ -1,2 +1,10 @@
 /// \mainpage Documentation de drunk_player
 ///
+///Drunk_player est un système de lecture de vidéos qui a trop bu. Il lit les vidéos contenues dans un dossier par morceaux, aléatoirement et parfois en transformant l'image.
+///
+///Drunk_player utilise la bibliothèque de traitement d'image <A HREF="opencv.org">OpenCV</A> et est composé :
+/// <UL>
+/// <LI> d'une bibliothèque (drunk_player) contenant le code de base </LI> 
+/// <LI> d'un programme graphique (drunk_player_gui) qui affiche le résultat à l'écran </LI>
+/// <LI> d'un programme console (drunk_player_cli) qui sort le résultat dans un fichie </LI>
+/// </UL>
